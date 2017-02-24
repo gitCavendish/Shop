89ac8643e3830474d68ab9b53bfd93f8c60a9b60
tinklexjane <tinklexjane@gmail.com>
2017-02-24 21:29:37 +0800

    complete category function

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,16 +18,16 @@ u.save
 puts '1 admin account created'
 
 products = [
-    { title: '西葫芦', description: '碳水化合物含量：7克（中等大小）', price: rand(10..100), image_path: "#{Rails.root}/app/assets/images/images.jpeg" },
-    { title: '菜花', description: '碳水化合物含量：每100克含5克', price: rand(5..50), image_path: "#{Rails.root}/app/assets/images/images-2.jpeg" },
-    { title: '蘑菇', description: '碳水化合物含量：每100克含3克', price: rand(5..50), image_path: "#{Rails.root}/app/assets/images/images-3.jpeg" },
-    { title: '芹菜', description: '碳水化合物含量：1克/根', price: rand(5..50), image_path: "#{Rails.root}/app/assets/images/images-4.jpeg" },
-    { title: '樱桃番茄', description: '碳水化合物含量：每100克含4克', price: rand(5..50), image_path: "#{Rails.root}/app/assets/images/images-5.jpeg" },
-    { title: '金丝瓜', description: '碳水化合物含量：每100克含7克', price: rand(5..50), image_path: "#{Rails.root}/app/assets/images/images-6.jpeg" },
-    { title: '杏', description: '碳水化合物含量：4克/个（中等大小）', price: rand(5..50), image_path: "#{Rails.root}/app/assets/images/images-7.jpeg" }
+    { title: '西葫芦', description: '碳水化合物含量：7克（中等大小）', price: rand(10..100), category: "chocolate", image_path: "#{Rails.root}/app/assets/images/images.jpeg" },
+    { title: '菜花', description: '碳水化合物含量：每100克含5克', price: rand(5..50), category: "chocolate", image_path: "#{Rails.root}/app/assets/images/images-2.jpeg" },
+    { title: '蘑菇', description: '碳水化合物含量：每100克含3克', price: rand(5..50), category: "nut", image_path: "#{Rails.root}/app/assets/images/images-3.jpeg" },
+    { title: '芹菜', description: '碳水化合物含量：1克/根', price: rand(5..50), category: "nut", image_path: "#{Rails.root}/app/assets/images/images-4.jpeg" },
+    { title: '樱桃番茄', description: '碳水化合物含量：每100克含4克', price: rand(5..50), category: "berry", image_path: "#{Rails.root}/app/assets/images/images-5.jpeg" },
+    { title: '金丝瓜', description: '碳水化合物含量：每100克含7克', price: rand(5..50), category: "freshfood", image_path: "#{Rails.root}/app/assets/images/images-6.jpeg" },
+    { title: '杏', description: '碳水化合物含量：4克/个（中等大小）', price: rand(5..50), category: "freshfood", image_path: "#{Rails.root}/app/assets/images/images-7.jpeg" }
 ]
 
 create_products = for i in 1..7 do
-                      Product.create!([title: products[i - 1][:title], description: products[i - 1][:description], price: rand(5..100), quantity: 100, image: open(products[i - 1][:image_path])])
+                      Product.create!([title: products[i - 1][:title], description: products[i - 1][:description], price: rand(5..100), quantity: 100, category: products[i-1][:category], image: open(products[i - 1][:image_path])])
 end
 puts '7 products created.'
