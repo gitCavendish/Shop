b2301049ef30c2842bf1680220d5facb323c79c3
Caven <xll290016537@gmail.com>
2017-02-25 14:04:23 +0800

    adjust checkout page & order show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170224083200) do
+ActiveRecord::Schema.define(version: 20170225020310) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20170224083200) do
     t.boolean  "is_paid",          default: false
     t.string   "payment_method"
     t.string   "aasm_state",       default: "order_placed"
+    t.string   "phone_number"
     t.index ["aasm_state"], name: "index_orders_on_aasm_state"
   end
 
